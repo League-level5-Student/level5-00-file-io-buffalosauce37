8e2e3ebee8b445b29f6a751f1be1409c079cdbf4
ilabstaff <ilabstaff@172.16.116.73>
2020-01-29 17:57:22 -0800

    bruh

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,23 @@
 package _01_File_Recorder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
-	// Create a program that takes a message from the user and saves it to a file.
-}
+	public static void main(String[] args) {
+	String s	=JOptionPane.showInputDialog("do it");
+		// Create a program that takes a message from the user and saves it to a file.
+		try {
+			FileWriter fw = new FileWriter("src/_00_Intro_To_File_Input_and_Output/test.txt", true);
+			
+			fw.write(s);
+				
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	}
+
